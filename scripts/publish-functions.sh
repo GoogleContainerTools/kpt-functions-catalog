1409ace0c1808e330ef54de08af8cfa2cc63fca7
Prachi Pendse <prachirp@google.com>
2020-03-16 19:03:44 -0700

    Address further review comments for helm-template

---
+++ scripts/publish-functions.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Publish shell functions to catalog.
+
+set -eo pipefail
+
+REPO="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
+TAG=${TAG:-dev}
+FUNCS=(helm-template)
+
+for FN_NAME in "${FUNCS[@]}"; do
+  DOCKER_IMAGE="gcr.io/kpt-functions/${FN_NAME}:${TAG}"
+  DOCKER_FILE="${REPO}/functions/${FN_NAME}/Dockerfile"
+  docker build -t "$DOCKER_IMAGE" -f "$DOCKER_FILE" functions
+	docker push "$DOCKER_IMAGE"
+done
--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 #
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # E2E tests for kpt-functions-catalog.
 
 set -eo pipefail
@@ -67,6 +81,7 @@ assert_dir_exists redis
 # kpt fn Tests
 ############################
 
+# TODO: Add kpt_helm_template_short test after kpt fn run --image fix <https://github.com/GoogleContainerTools/kpt/issues/359>
 testcase "kpt_helm_template_imperative"
 kpt fn source example-configs |
   docker run -i -v "$(pwd)/${CHARTS_SRC}":/source gcr.io/kpt-functions/helm-template chart_path=/source/mongodb name=my-mongodb |
