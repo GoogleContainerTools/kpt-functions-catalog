10c8a322a19f39668897bf1cbc2a366212d54049
Natasha Sarkar <natashasarkar@google.com>
2021-04-20 22:02:58 -0700

    rewrite set-label with openapi

--- functions/go/set-labels/main.go
+++ functions/go/set-labels/main.go
@@ -10,8 +10,12 @@ import (
 	"sigs.k8s.io/kustomize/api/resmap"
 	"sigs.k8s.io/kustomize/api/resource"
 	"sigs.k8s.io/kustomize/api/types"
-	"sigs.k8s.io/kustomize/kyaml/fn/framework"
+
 	"sigs.k8s.io/kustomize/kyaml/fn/framework/command"
+
+	"sigs.k8s.io/kustomize/kyaml/fn/framework"
+	"sigs.k8s.io/kustomize/kyaml/openapi"
+
 	kyaml "sigs.k8s.io/kustomize/kyaml/yaml"
 	"sigs.k8s.io/yaml"
 )
@@ -21,6 +25,10 @@ const (
 	fnConfigVersion    = "v1alpha1"
 	fnConfigAPIVersion = fnConfigGroup + "/" + fnConfigVersion
 	fnConfigKind       = "SetLabelConfig"
+
+	FieldMeaningExtension = "x-kubernetes-field-meaning"
+	Label                 = "label"
+)
 )
 
 //nolint
@@ -62,20 +70,20 @@ type transformerConfig struct {
 
 type setLabelFunction struct {
 	kyaml.ResourceMeta `json:",inline" yaml:",inline"`
-	plugin             `json:",inline" yaml:",inline"`
+	Labels     map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
 }
 
 func (f *setLabelFunction) Config(rn *kyaml.RNode) error {
 	switch {
 	case f.validGVK(rn, "v1", "ConfigMap"):
-		f.plugin.Labels = rn.GetDataMap()
+		f.Labels = rn.GetDataMap()
 	case f.validGVK(rn, fnConfigAPIVersion, fnConfigKind):
 		// input config is a CRD
 		y, err := rn.String()
 		if err != nil {
 			return fmt.Errorf("cannot get YAML from RNode: %w", err)
 		}
-		err = yaml.Unmarshal([]byte(y), &f.plugin)
+		err = yaml.Unmarshal([]byte(y), &f)
 		if err != nil {
 			return fmt.Errorf("failed to unmarshal config %#v: %w", y, err)
 		}
@@ -83,30 +91,94 @@ func (f *setLabelFunction) Config(rn *kyaml.RNode) error {
 		return fmt.Errorf("function config must be a ConfigMap or %s", fnConfigKind)
 	}
 
-	if len(f.plugin.Labels) == 0 {
+	if len(f.Labels) == 0 {
 		return fmt.Errorf("input label list cannot be empty")
 	}
-	tc, err := getDefaultConfig()
-	if err != nil {
-		return err
-	}
-	// append default field specs
-	f.plugin.FieldSpecs = append(f.plugin.FieldSpecs, tc.FieldSpecs...)
 	return nil
 }
 
 func (f *setLabelFunction) Run(items []*kyaml.RNode) ([]*kyaml.RNode, error) {
-	resmapFactory := newResMapFactory()
-	resMap, err := resmapFactory.NewResMapFromRNodeSlice(items)
-	if err != nil {
-		return nil, err
+	for _, r := range items {
+		meta, _ := r.GetMeta()
+		if meta.APIVersion == "kpt.dev/v1" && meta.Kind == "OpenAPI" {
+			openapi.SuppressBuiltInSchemaUse()
+			schema, err := r.Pipe(kyaml.Lookup("data"))
+			if err != nil {
+				return nil, fmt.Errorf("could not configure OpenAPI schema")
+			}
+			str, err := schema.String()
+			if err != nil {
+				return nil, err
+			}
+			json, err := yaml.YAMLToJSON([]byte(str))
+			if err != nil {
+				return nil, err
+			}
+			if err = openapi.AddSchema(json); err != nil {
+				return nil, err
+			}
+		}
 	}
-	err = f.plugin.Transform(resMap)
-	if err != nil {
-		return nil, fmt.Errorf("failed to run transformer: %w", err)
+
+	var result []*kyaml.RNode
+	for _, r := range items {
+		meta, _ := r.GetMeta()
+		rs := openapi.SchemaForResourceType(meta.TypeMeta)
+		if err := f.updateNode(r, rs); err != nil {
+			return nil, err
+		}
+		result = append(result, r)
 	}
-	return resMap.ToRNodeSlice()
+	return result, nil
 }
+
+func (f *setLabelFunction) updateNode(node *kyaml.RNode, rs *openapi.ResourceSchema) error {
+	if rs == nil || node == nil {
+		return nil
+	}
+
+	for _, path := range getPathsToLabels(rs) {
+		l, err := node.Pipe(kyaml.LookupCreate(kyaml.MappingNode, path...))
+		if err != nil {
+			return err
+		}
+
+		labels := make(map[string]interface{})
+		_ = l.VisitFields(func(node *kyaml.MapNode) error {
+			labels[kyaml.GetValue(node.Key)] = kyaml.GetValue(node.Value)
+			return nil
+		})
+		for k, v := range f.Labels {
+			labels[k] = v
+		}
+
+		n, err := kyaml.FromMap(labels)
+		if err != nil {
+			return err
+		}
+
+		l.SetYNode(n.YNode())
+	}
+
+	return nil
+}
+
+func getPathsToLabels(rs *openapi.ResourceSchema) [][]string {
+	var result [][]string
+
+	fieldMeaning, found := rs.Schema.Extensions[FieldMeaningExtension]
+	if found && fieldMeaning == Label {
+		result = append(result, []string{})
+	}
+
+	for field := range rs.Schema.Properties {
+		for _, path := range getPathsToLabels(rs.Field(field)) {
+			result = append(result, append([]string{field}, path...))
+		}
+	}
+	return result
+}
+
 
 func (f *setLabelFunction) validGVK(rn *kyaml.RNode, apiVersion, kind string) bool {
 	meta, err := rn.GetMeta()
@@ -126,10 +198,6 @@ func getDefaultConfig() (transformerConfig, error) {
 	return tc, err
 }
 
-func newResMapFactory() *resmap.Factory {
-	resourceFactory := resource.NewFactory(kunstruct.NewKunstructuredFactoryImpl())
-	return resmap.NewFactory(resourceFactory, nil)
-}
 
 func run(resourceList *framework.ResourceList) error {
 	var fn setLabelFunction
@@ -137,7 +205,6 @@ func run(resourceList *framework.ResourceList) error {
 	if err != nil {
 		return fmt.Errorf("failed to configure function: %w", err)
 	}
-
 	resourceList.Items, err = fn.Run(resourceList.Items)
 	if err != nil {
 		return fmt.Errorf("failed to run function: %w", err)
