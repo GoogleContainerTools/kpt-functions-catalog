ae6c687f9411bab9404348add75699311ee90d34
Mengqi Yu <mengqiy@google.com>
2021-04-29 16:55:49 -0700

    add usage doc for kubeval (#257)
    * add usage doc for kubeval
    
    * update usage doc
    
    * update usage doc
    
    * update usage doc
    
    * update usage doc

--- functions/ts/kubeval/src/kubeval.ts
+++ functions/ts/kubeval/src/kubeval.ts
@@ -225,21 +225,43 @@ function readStdoutToString(childProcess: ChildProcess): Promise<string> {
 }
 
 kubeval.usage = `
-Validates configuration using kubeval.
+Use kubeval to validate KRM resources against their json schemas.
+
+The function configuration must be a ConfigMap.
+
+The following keys can be used in the data field of the ConfigMap, and all of
+them are optional:
+
+${SCHEMA_LOCATION}: The base URL used to fetch the json schemas. The default is
+  empty. This feature only works with imperative runs, since declarative runs
+  allow neither network access nor volume mount.
+${ADDITIONAL_SCHEMA_LOCATIONS}: List of secondary base URLs used to fetch the
+  json schemas.  These URLs will be used if the URL specified by
+  ${SCHEMA_LOCATION} did not have the required schema.  The default is empty.
+  This feature only works with imperative runs.
+${IGNORE_MISSING_SCHEMAS}: Skip validation for resources without a schema. The
+  default is false.
+${SKIP_KINDS}: Comma-separated list of case-sensitive kinds to skip when
+  validating against schemas. The default is empty.
+${STRICT}: Disallow additional properties that are not in the schemas. The
+  default is false.
+
+The following is an example function configuration:
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: my-func-config
+data:
+  schema_location: "https://kubernetesjsonschema.dev"
+  additional_schema_locations: "https://example.com,file:///abs/path/to/your/schema/directory"
+  ignore_missing_schemas: "false"
+  skip_kinds: "DaemonSet,MyCRD"
+  strict: "true"
 
 If neither ${SCHEMA_LOCATION} nor ${ADDITIONAL_SCHEMA_LOCATIONS} is provided, we
-will use the baked-in schemas. Otherwise, we will try to download the schemas.
-
-Configured using a ConfigMap with the following keys, all of which are optional:
-${SCHEMA_LOCATION}: The base URL used to download schemas.
-${ADDITIONAL_SCHEMA_LOCATIONS}: List of secondary base URLs used to download
-  schemas.  These URLs will be used if the URL specified by ${SCHEMA_LOCATION}
-  did not have the required schema.  By default, there are no secondary URLs,
-  and only the primary base URL will be used.
-${IGNORE_MISSING_SCHEMAS}: Skip validation for resource definitions without a
-  schema.  If omitted, a default value of false will be assumed.
-${SKIP_KINDS}: Comma-separated list of case-sensitive kinds to skip when
-  validating against schemas.  If omitted, no kinds will be skipped.
-${STRICT}: Disallow additional properties not in schema.  If omitted, a default
-  value of false will be assumed.
+will convert the baked-in OpenAPI document to json schemas and use them.
+The baked-in OpenAPI document is from a GKE cluster with version v1.19.8. The
+OpenAPI document contains kubernetes built-in types and some GCP CRDs (e.g.
+BackendConfig), but it currently doesn't contain Config Connector CRDs.
 `;
