e8f7a052084f61615ba5deebdc66ab7202d0378f
phani <phanitejamarupaka@gmail.com>
2021-06-29 12:33:05 -0700

    Update search replace example (#449)
    * Update search replace example
    
    * Update link

--- examples/search-replace-create-setters/.expected/exec.sh
+++ examples/search-replace-create-setters/.expected/exec.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 # shellcheck disable=SC2016
-kpt fn eval --image gcr.io/kpt-fn/search-replace:v0.1 -- 'by-path=spec.replicas' 'put-comment=kpt-set: ${replicas}'
-
-# shellcheck disable=SC2016
-kpt fn eval --image gcr.io/kpt-fn/search-replace:v0.1 -- 'by-path=spec.**.image' 'put-comment=kpt-set: gcr.io/${image}:${tag}'
+kpt fn eval --image gcr.io/kpt-fn/search-replace:v0.1 -- \
+by-path='data.**' by-value-regex='(.*)nginx.com(.*)' put-comment='kpt-set: ${1}${host}${2}'
