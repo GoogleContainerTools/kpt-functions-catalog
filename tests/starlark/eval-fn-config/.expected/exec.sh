2f18c351c014c3fbd5d03dc99003048c2bcc62dc
Mengqi Yu <mengqiy@google.com>
2021-07-13 13:23:31 -0700

    drop dangling Kptfile

--- tests/starlark/eval-fn-config/.expected/exec.sh
+++ tests/starlark/eval-fn-config/.expected/exec.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-kpt fn eval --image gcr.io/kpt-fn/starlark:unstable --fn-config set-ns.yaml --image-pull-policy never
+kpt fn eval --image gcr.io/kpt-fn/starlark:unstable --fn-config fn-config.yaml --image-pull-policy never
