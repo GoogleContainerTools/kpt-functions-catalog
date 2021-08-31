494daf20faddced0dbb72acbad90e2d00a8bdd62
Mengqi Yu <mengqiy@google.com>
2021-08-31 09:40:10 -0700

    Dummy update to trigger the checkSite CI

--- scripts/generate_catalog/generate_catalog.go
+++ scripts/generate_catalog/generate_catalog.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 // Usage: generate_catalog SRC_REPO_DIR/ DEST_MD_DIR/
 //
 // The command will create a README.md file under DEST_MD_DIR/ containing a table
