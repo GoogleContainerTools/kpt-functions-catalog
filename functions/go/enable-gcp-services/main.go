47289327e1316669a86228f56f1890a282b3e53b
bharathkkb <bharathkrishnakb@gmail.com>
2021-09-23 12:16:04 -0500

    sort result items

--- functions/go/enable-gcp-services/main.go
+++ functions/go/enable-gcp-services/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/GoogleContainerTools/kpt-functions-catalog/functions/go/project-services/gcpservices"
 	"github.com/GoogleContainerTools/kpt-functions-catalog/functions/go/project-services/generated"
@@ -40,6 +41,7 @@ func (psf *ProjectServiceSetFunction) Process(resourceList *framework.ResourceLi
 		return err
 	}
 	resourceList.Result.Items = pslr.GetResults()
+	sortResultItems(resourceList.Result.Items)
 	return nil
 }
 
@@ -51,4 +53,31 @@ func getErrorItem(errMsg string) []framework.ResultItem {
 			Severity: framework.Error,
 		},
 	}
+}
+
+// from https://github.com/GoogleContainerTools/kpt/issues/2195
+// refactor once upstreamed
+func sortResultItems(items []framework.ResultItem) {
+	sort.SliceStable(items, func(i, j int) bool {
+		if fileLess(items, i, j) != 0 {
+			return fileLess(items, i, j) < 0
+		}
+		return resultItemToString(items[i]) < resultItemToString(items[j])
+	})
+}
+
+func fileLess(items []framework.ResultItem, i, j int) int {
+	if items[i].File.Path != items[j].File.Path {
+		if items[i].File.Path < items[j].File.Path {
+			return -1
+		} else {
+			return 1
+		}
+	}
+	return items[i].File.Index - items[j].File.Index
+}
+
+func resultItemToString(item framework.ResultItem) string {
+	return fmt.Sprintf("resource-ref:%s,field:%s,message:%s",
+		item.ResourceRef, item.Field, item.Message)
 }
