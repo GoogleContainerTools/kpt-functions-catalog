ddbd9ddbc75cf4da9c8861ebb7e5d31ac246bcb4
Mengqi Yu <mengqiy@google.com>
2021-10-14 11:59:33 -0700

    update for kubeval/v0.2 (#620)

--- examples/kubeval-mount-schema/.expected/exec.sh
+++ examples/kubeval-mount-schema/.expected/exec.sh
@@ -2,6 +2,6 @@
 
 set -eo pipefail
 
-kpt fn eval -i gcr.io/kpt-fn/kubeval:unstable --image-pull-policy never \
+kpt fn eval -i gcr.io/kpt-fn/kubeval:v0.2 --image-pull-policy never \
   --mount type=bind,src="$(pwd)/jsonschema",dst=/schema-dir/master-standalone \
   -- schema_location=file:///schema-dir
