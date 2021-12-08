fae0a45ebd3d47c8a7e3931dabb2d42b198da897
sdowell <sdowell@google.com>
2021-12-07 21:54:19 -0800

    test: Fix e2e tests (#667)
    These tests were broken due to upstream changes. The stderr
    output includes additional output which would be fragile to
    capture in a test case, so the assertion is changed to
    results.yaml.
    
    The contrib test is being skipped for now pending further
    investigation.

--- examples/kubeval-mount-schema/.expected/exec.sh
+++ examples/kubeval-mount-schema/.expected/exec.sh
@@ -3,5 +3,6 @@
 set -eo pipefail
 
 kpt fn eval -i gcr.io/kpt-fn/kubeval:unstable --image-pull-policy never \
+  --results-dir="$(pwd)/../results" \
   --mount type=bind,src="$(pwd)/jsonschema",dst=/schema-dir/master-standalone \
   -- schema_location=file:///schema-dir
