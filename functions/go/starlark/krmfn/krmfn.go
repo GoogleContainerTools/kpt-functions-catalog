9ca1ad2e40e8e2b9bbd7fdf3f58240b656552c3c
natasha41575 <natashasarkar@google.com>
2022-03-29 17:31:18 -0700

    [starlark] builtin match_gvkn function

---
+++ functions/go/starlark/krmfn/krmfn.go
@@ -0,0 +1,33 @@
+package krmfn
+
+import (
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+const ModuleName = "krmfn.star"
+
+var Module = &starlarkstruct.Module{
+	Name: "krmfn",
+	Members: starlark.StringDict{
+		"match_gvkn": starlark.NewBuiltin("match_gvkn", matchGVKN),
+	},
+}
+
+func matchGVKN(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var resourceList starlark.Value
+	var apiVersion, kind, name string
+	if err := starlark.UnpackPositionalArgs("match_gvkn", args, kwargs, 4,
+		&resourceList, &apiVersion, &kind, &name); err != nil {
+		return nil, err
+	}
+	obj, err := fn.ParseKubeObject([]byte(resourceList.String()))
+	if err != nil {
+		return nil, err
+	}
+	if obj.GetAPIVersion() == apiVersion && obj.GetKind() == kind && obj.GetName() == name {
+		return starlark.Bool(true), nil
+	}
+	return starlark.Bool(false), nil
+}
