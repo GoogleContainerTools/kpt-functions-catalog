f4d5da1a4e82990449cca15920b5b149472ac640
natasha41575 <natashasarkar@google.com>
2022-04-04 16:34:24 -0700

    suggested changes

--- functions/go/starlark/krmfn/krmfn.go
+++ functions/go/starlark/krmfn/krmfn.go
@@ -11,21 +11,48 @@ const ModuleName = "krmfn.star"
 var Module = &starlarkstruct.Module{
 	Name: "krmfn",
 	Members: starlark.StringDict{
-		"match_gvkn": starlark.NewBuiltin("match_gvkn", matchGVKN),
+		"match_gvk":       starlark.NewBuiltin("match_gvk", matchGVK),
+		"match_name":      starlark.NewBuiltin("match_name", matchName),
+		"match_namespace": starlark.NewBuiltin("match_namespace", matchNamespace),
 	},
 }
 
-func matchGVKN(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+func matchGVK(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
 	var resourceList starlark.Value
-	var apiVersion, kind, name string
-	if err := starlark.UnpackPositionalArgs("match_gvkn", args, kwargs, 4,
-		&resourceList, &apiVersion, &kind, &name); err != nil {
+	var apiVersion, kind string
+	if err := starlark.UnpackPositionalArgs("match_gvk", args, kwargs, 3,
+		&resourceList, &apiVersion, &kind); err != nil {
 		return nil, err
 	}
 	obj, err := fn.ParseKubeObject([]byte(resourceList.String()))
 	if err != nil {
 		return nil, err
 	}
-	match := obj.GetAPIVersion() == apiVersion && obj.GetKind() == kind && obj.GetName() == name
-	return starlark.Bool(match), nil
+	return starlark.Bool(obj.IsGVK(apiVersion, kind)), nil
+}
+
+func matchName(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var resourceList starlark.Value
+	var name string
+	if err := starlark.UnpackPositionalArgs("match_name", args, kwargs, 2, &resourceList, &name); err != nil {
+		return nil, err
+	}
+	obj, err := fn.ParseKubeObject([]byte(resourceList.String()))
+	if err != nil {
+		return nil, err
+	}
+	return starlark.Bool(obj.GetName() == name), nil
+}
+
+func matchNamespace(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var resourceList starlark.Value
+	var namespace string
+	if err := starlark.UnpackPositionalArgs("match_namespace", args, kwargs, 2, &resourceList, &namespace); err != nil {
+		return nil, err
+	}
+	obj, err := fn.ParseKubeObject([]byte(resourceList.String()))
+	if err != nil {
+		return nil, err
+	}
+	return starlark.Bool(obj.GetNamespace() == namespace), nil
 }
