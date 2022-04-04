0c3cc5c535b8378fd9b76452f7a3319e713f15f5
droot <sunilarora@google.com>
2022-04-04 14:37:23 -0700

    adjusted search-replace tests to match kpt behavior to include meta resources by default

--- tests/search-replace/file-path-match/.expected/exec.sh
+++ tests/search-replace/file-path-match/.expected/exec.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # shellcheck disable=SC2016
-kpt fn eval --image gcr.io/kpt-fn/search-replace:unstable --include-meta-resources --image-pull-policy=never -- \
+kpt fn eval --image gcr.io/kpt-fn/search-replace:unstable --image-pull-policy=never -- \
 by-value=project-id by-file-path='**/setters.yaml' put-value=new-project
