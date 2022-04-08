89698ce992cef4f00b3803b77e2faf2675883712
Bharath KKB <bharathkrishnakb@gmail.com>
2022-04-08 17:43:14 -0500

    chore: move generate-kpt-pkg-docs to curated (#799)
    * chore: move generate-kpt-pkg-docs to curated
    
    * fix metadata
    
    * fix test

--- contrib/examples/generate-kpt-pkg-docs-simple/.expected/exec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-kpt fn eval -i gcr.io/kpt-fn-contrib/generate-kpt-pkg-docs:unstable --image-pull-policy never \
---mount type=bind,src="$(pwd)",dst=/tmp,rw=true --as-current-user -- readme-path=/tmp/GENERATED.md
