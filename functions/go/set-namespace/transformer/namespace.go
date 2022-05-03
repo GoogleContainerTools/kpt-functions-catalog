c6fbf187ac65d72b8da3665b2b7bcc8a42e4cb8f
Yuwen Ma <yuwenma@google.com>
2022-05-02 20:52:32 -0700

    always update namespace vwithout restricting the original namespace value; update k8s resources with accurate scope info, and update crd resources with the  metadata.namespace hypothesis

--- functions/go/set-namespace/transformer/namespace.go
+++ functions/go/set-namespace/transformer/namespace.go
@@ -19,7 +19,6 @@ import (
 	"strings"
 
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func SetNamespace(rl *fn.ResourceList) (bool, error) {
@@ -31,32 +30,39 @@ func SetNamespace(rl *fn.ResourceList) (bool, error) {
 		return true, nil
 	}
 	// Update "namespace" to the proper resources.
-	count := tc.Transform(rl.Items)
-	var result *fn.Result
-	if len(tc.Errors) != 0 {
-		errMsg := strings.Join(tc.Errors, "\n")
-		result = fn.GeneralResult(errMsg, fn.Error)
-	} else {
-		result = fn.GeneralResult(tc.LogResult(count), fn.Info)
-	}
-
+	oldNamespaces := map[string]struct{}{}
+	count := tc.Transform(rl.Items, oldNamespaces)
+	result := tc.LogResults(rl, count, oldNamespaces)
 	rl.Results = append(rl.Results, result)
 	return true, nil
 }
 
-func (p *NamespaceTransformer) LogResult(count int) string {
-	return fmt.Sprintf("namespace %q updated to %q, %d values changed", p.NamespaceMatcher, p.NewNamespace,
-		count)
+func (p *NamespaceTransformer) LogResults(rl *fn.ResourceList, count int, oldNamespaces map[string]struct{}) *fn.Result {
+	if len(p.Errors) != 0 {
+		errMsg := strings.Join(p.Errors, "\n")
+		return fn.GeneralResult(errMsg, fn.Error)
+	}
+	if dupErr := fn.CheckResourceDuplication(rl); dupErr != nil {
+		return fn.ErrorResult(dupErr)
+	}
+	if count == 0 {
+		msg := fmt.Sprintf("all namespaces are already %q. no value changed", p.NewNamespace)
+		return fn.GeneralResult(msg, fn.Info)
+	}
+
+	msg := fmt.Sprintf("namespace %q updated to %q, %d value(s) changed", reflect.ValueOf(oldNamespaces).MapKeys(),
+		p.NewNamespace, count)
+	return fn.GeneralResult(msg, fn.Info)
 }
 
 // Config gets the attributes from different FunctionConfig formats.
 func (p *NamespaceTransformer) Config(o *fn.KubeObject) error {
 	switch {
 	case o.IsGVK("v1", "ConfigMap"):
-		p.NewNamespace = o.GetStringOrDie("data", "namespace")
+		p.NewNamespace = o.NestedStringOrDie("data", "namespace")
 		if p.NewNamespace == "" {
 			if o.GetName() == "kptfile.kpt.dev" {
-				p.NewNamespace = o.GetStringOrDie("data", "name")
+				p.NewNamespace = o.NestedStringOrDie("data", "name")
 				if p.NewNamespace == "" {
 					return fmt.Errorf("`data.name` should not be empty")
 				}
@@ -64,13 +70,11 @@ func (p *NamespaceTransformer) Config(o *fn.KubeObject) error {
 				return fmt.Errorf("`data.namespace` should not be empty")
 			}
 		}
-		p.NamespaceMatcher = o.GetStringOrDie("data", "namespaceMatcher")
 	case o.IsGVK(fnConfigAPIVersion, fnConfigKind):
-		p.NewNamespace = o.GetStringOrDie("namespace")
+		p.NewNamespace = o.NestedStringOrDie("namespace")
 		if p.NewNamespace == "" {
 			return fmt.Errorf("`namespace` should not be empty")
 		}
-		p.NamespaceMatcher = o.GetStringOrDie("data", "namespaceMatcher")
 	default:
 		return fmt.Errorf("unknown functionConfig Kind=%v ApiVersion=%v, expect `%v` or `ConfigMap`",
 			o.GetKind(), o.GetAPIVersion(), fnConfigKind)
@@ -80,15 +84,12 @@ func (p *NamespaceTransformer) Config(o *fn.KubeObject) error {
 
 // Transform replace the existing Namespace object, namespace-scoped resources' `metadata.name` and
 // other namespace reference fields to the new value.
-func (p *NamespaceTransformer) Transform(objects []*fn.KubeObject) int {
+func (p *NamespaceTransformer) Transform(objects []*fn.KubeObject, oldNamespaces map[string]struct{}) int {
 	count := new(int)
-	namespaces, nsObjCounter := FindAllNamespaces(objects)
-	if oldNs, ok := p.GetOldNamespace(namespaces, nsObjCounter); ok {
-		ReplaceNamespace(objects, oldNs, p.NewNamespace, count)
-		// Update the resources annotation "config.kubernetes.io/depends-on" which may contain old namespace value.
-		dependsOnMap := GetDependsOnMap(objects)
-		UpdateAnnotation(objects, oldNs, p.NewNamespace, dependsOnMap)
-	}
+	ReplaceNamespace(objects, p.NewNamespace, count, oldNamespaces)
+	// Update the resources annotation "config.kubernetes.io/depends-on" which may contain old namespace value.
+	dependsOnMap := GetDependsOnMap(objects)
+	UpdateAnnotation(objects, p.NewNamespace, oldNamespaces, dependsOnMap)
 	return *count
 }
 
@@ -100,68 +101,47 @@ func VisitNamespaces(objects []*fn.KubeObject, visitor func(namespace *Namespace
 		case o.IsLocalConfig():
 			continue
 		case o.IsGVK("v1", "Namespace"):
-			namespace := o.GetStringOrDie("metadata", "name")
+			namespace := o.NestedStringOrDie("metadata", "name")
 			visitor(NewNamespace(o, &namespace))
 			o.SetOrDie(&namespace, "metadata", "name")
 		case o.IsGVK("apiextensions.k8s.io/v1", "CustomResourceDefinition"):
-			namespace := o.GetStringOrDie("spec", "conversion", "webhook", "clientConfig", "service", "namespace")
+			namespace := o.NestedStringOrDie("spec", "conversion", "webhook", "clientConfig", "service", "namespace")
 			visitor(NewNamespace(o, &namespace))
 			o.SetOrDie(&namespace, "spec", "conversion", "webhook", "clientConfig", "service", "namespace")
 		case o.IsGVK("apiregistration.k8s.io/v1", "APIService"):
-			namespace := o.GetStringOrDie("spec", "service", "namespace")
+			namespace := o.NestedStringOrDie("spec", "service", "namespace")
 			visitor(NewNamespace(o, &namespace))
 			o.SetOrDie(&namespace, "spec", "service", "namespace")
 		case o.GetKind() == "ClusterRoleBinding" || o.GetKind() == "RoleBinding":
 			subjects := o.GetSlice("subjects")
 			for _, s := range subjects {
+				// Note: Users may not expect to change the subject namespace fields. We shall provide an option
+				// to skip changing the namespace in ClusterRolebinding/Rolebinding subjects.
+				// See https://github.com/kubernetes-sigs/kustomize/issues/1599
 				var ns string
 				found, _ := s.Get(&ns, "namespace")
 				if found {
 					visitor(NewNamespace(o, &ns))
-					_ = s.Set(&ns, "namespace")
+					_ = s.SetNestedField(&ns, "namespace")
 				}
 			}
 			o.SetOrDie(&subjects, "subjects")
-		default:
-			// skip the cluster scoped resource. We determine if a resource is cluster scoped by
-			// checking if the metadata.namespace is configured.
 		}
-		if o.HasNamespace() {
-			// We made the hypothesis that a namespace scoped resource should have its metadata.namespace field setup.
-			// All above special types are cluster scoped, except RoleBinding
-			namespace := o.GetStringOrDie("metadata", "namespace")
+		// "IsNamespaceScoped" detects resource scope by checking whether `metadata.namespace` is set.
+		// This is valid hypothesis is for custom k8s resources. For k8s resources, the function always gives accurate scope information.
+		if o.IsNamespaceScoped() {
+			namespace := o.NestedStringOrDie("metadata", "namespace")
 			visitor(NewNamespace(o, &namespace))
 			o.SetOrDie(&namespace, "metadata", "namespace")
 		}
 	}
 }
 
-// FindAllNamespaces iterates the `objects` to list all namespaces and count the number of Namespace objects.
-func FindAllNamespaces(objects []*fn.KubeObject) ([]string, map[string]int) {
-	nsObjCounter := map[string]int{}
-	namespaces := sets.NewString()
-	VisitNamespaces(objects, func(ns *Namespace) {
-		if *ns.Ptr == "" {
-			return
-		}
-		if ns.IsNamespace {
-			nsObjCounter[*ns.Ptr] += 1
-		}
-		namespaces.Insert(*ns.Ptr)
-	})
-	return namespaces.List(), nsObjCounter
-}
-
 // ReplaceNamespace iterates the `objects` to replace the `OldNs` with `newNs` on namespace field.
-func ReplaceNamespace(objects []*fn.KubeObject, oldNs, newNs string, count *int) {
-	if oldNs == newNs {
-		return
-	}
+func ReplaceNamespace(objects []*fn.KubeObject, newNs string, count *int, oldNamespace map[string]struct{}) {
 	VisitNamespaces(objects, func(ns *Namespace) {
-		if *ns.Ptr == "" {
-			return
-		}
-		if *ns.Ptr == oldNs {
+		if *ns.Ptr != newNs {
+			oldNamespace[*ns.Ptr] = struct{}{}
 			*ns.Ptr = newNs
 			*count += 1
 		}
@@ -179,7 +159,7 @@ func GetDependsOnMap(objects []*fn.KubeObject) map[string]bool {
 }
 
 // UpdateAnnotation updates the `objects`'s "config.kubernetes.io/depends-on" annotation which contains namespace value.
-func UpdateAnnotation(objects []*fn.KubeObject, oldNs, newNs string, dependsOnMap map[string]bool) {
+func UpdateAnnotation(objects []*fn.KubeObject, newNs string, oldNs map[string]struct{}, dependsOnMap map[string]bool) {
 	VisitNamespaces(objects, func(ns *Namespace) {
 		if ns.DependsOnAnnotation == "" || !namespacedResourcePattern.MatchString(ns.DependsOnAnnotation) {
 			return
@@ -187,7 +167,7 @@ func UpdateAnnotation(objects []*fn.KubeObject, oldNs, newNs string, dependsOnMa
 		segments := strings.Split(ns.DependsOnAnnotation, "/")
 		dependsOnkey := dependsOnKeyPattern(segments[groupIdx], segments[kindIdx], segments[nameIdx])
 		if ok := dependsOnMap[dependsOnkey]; ok {
-			if segments[namespaceIdx] == oldNs {
+			if _, ok := oldNs[segments[namespaceIdx]]; ok {
 				segments[namespaceIdx] = newNs
 				newAnnotation := strings.Join(segments, "/")
 				ns.SetDependsOnAnnotation(newAnnotation)
@@ -196,60 +176,10 @@ func UpdateAnnotation(objects []*fn.KubeObject, oldNs, newNs string, dependsOnMa
 	})
 }
 
-// GetOldNamespace finds the existing namespace and make sure the input resourceList.items satisfy the requirements.
-// - no more than one Namespace Object can exist in the input resource.items
-// - If Namespace object exists, its name is the `oldNs`
-// - If `namespaceMatcher` is given, its value is the `oldNs`
-// - If neither Namespace object nor `namespaceMatcher` found, all resources should have the same namespace value and
-// this value is teh `oldNs`
-func (p *NamespaceTransformer) GetOldNamespace(fromResources []string, nsCount map[string]int) (string, bool) {
-	if p.NamespaceMatcher != "" {
-		if len(nsCount) == 0 {
-			return p.NamespaceMatcher, true
-		}
-		p.Errors = append(p.Errors,
-			"found Namespace objects from the input resources, "+
-				"you cannot use `namespaceMatcher` in FunctionConfig together with Namespace objects")
-		return "", false
-	}
-	if len(nsCount) > 1 {
-		msg := fmt.Sprintf("cannot accept more than one Namespace objects from the input resources, found %v",
-			nsCount)
-		p.Errors = append(p.Errors, msg)
-		return "", false
-	}
-	if len(nsCount) == 1 {
-		// Use the namespace object as the matching namespace if `namespaceMatcher` is not given.
-		oldNs := reflect.ValueOf(nsCount).MapKeys()[0].String()
-		if nsCount[oldNs] > 1 {
-			msg := fmt.Sprintf("found more than one Namespace objects of the same name %q", oldNs)
-			p.Errors = append(p.Errors, msg)
-			return "", false
-		}
-		p.NamespaceMatcher = oldNs
-		return oldNs, true
-	}
-	if len(fromResources) > 1 {
-		msg := fmt.Sprintf("all input namespace-scoped resources should be under the same namespace "+
-			"but found different namespaces: %q ", strings.Join(fromResources, ","))
-		p.Errors = append(p.Errors, msg)
-		return "", false
-	}
-	if len(fromResources) == 0 {
-		msg := "could not find any namespace fields to update. This function requires at least one of Namespace objects or " +
-			"namespace-scoped resources to have their namespace field set."
-		p.Errors = append(p.Errors, msg)
-		return "", false
-	}
-	p.NamespaceMatcher = fromResources[0]
-	return fromResources[0], true
-}
-
 type NamespaceTransformer struct {
-	NewNamespace     string
-	NamespaceMatcher string
-	DependsOnMap     map[string]bool
-	Errors           []string
+	NewNamespace string
+	DependsOnMap map[string]bool
+	Errors       []string
 }
 
 func NewNamespace(obj *fn.KubeObject, namespacePtr *string) *Namespace {
