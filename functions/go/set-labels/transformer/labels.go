7d1da0646aa3ac0d5da11db652d6c40ba874f0fb
Yuanyi <zyuanyi@google.com>
2022-06-29 13:54:54 -0500

    update test cases format and revert back to resourcelist version

--- functions/go/set-labels/transformer/labels.go
+++ functions/go/set-labels/transformer/labels.go
@@ -2,74 +2,11 @@ package transformer
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	"log"
-	"sigs.k8s.io/yaml"
 	"sort"
 	"strings"
 )
-
-// implement runner
-type SetLabels struct {
-	Labels              map[string]string `json:"labels,omitempty"`
-	AdditionalFieldSpec []FieldSpec       `json:"additionalLabelFields,omitempty"`
-}
-
-// Run the method that every runner need to implement
-// TODO: change e2e test cases to match runner: empty config error.
-func (r *SetLabels) Run(ctx *fn.Context, functionConfig *fn.KubeObject, items []*fn.KubeObject) {
-	if len(r.Labels) == 0 {
-		errStr := "failed to configure function: input label list cannot be empty"
-		if ctx != nil {
-			ctx.Result(errStr, fn.Error)
-		}
-		fn.Logf(errStr)
-		return
-	}
-	if err := r.Transform(items, ctx); err != nil {
-		if ctx != nil {
-			ctx.ResultErr(err.Error(), nil)
-		}
-		fn.Logf(err.Error())
-		return
-	}
-}
-
-func (r *SetLabels) Transform(objects fn.KubeObjects, ctx *fn.Context) error {
-	for _, o := range objects {
-		if err := visitByDefaultSetting(o, ctx, r.Labels); err != nil {
-			return err
-		}
-		if err := visitBySpecArr(o, r.AdditionalFieldSpec, ctx, r.Labels); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func visitByDefaultSetting(obj *fn.KubeObject, ctx *fn.Context, newLabels map[string]string) error {
-	commonFields := readDefaultFieldSpec()
-	return visitBySpecArr(obj, commonFields, ctx, newLabels)
-}
-
-func visitBySpecArr(obj *fn.KubeObject, specs []FieldSpec, ctx *fn.Context, newLabels map[string]string) error {
-	for _, sp := range specs {
-		if (sp.Kind == "" && sp.Version == "" && sp.Group == "") || obj.IsGVK(sp.Group, sp.Version, sp.Kind) {
-			// generate msg
-			err := updateLabels(obj, newLabels, sp)
-			if err != nil {
-				return err
-			}
-			// success, record result
-			res, _ := json.Marshal(newLabels)
-			msg := "set labels: " + string(res)
-			if ctx != nil {
-				ctx.ResultInfo(msg, obj)
-			}
-		}
-	}
-	return nil
-}
 
 // the similar struct esxits in resid.GVK, but there is no function to create an GVK struct without using kyaml
 type FieldSpec struct {
@@ -80,9 +17,111 @@ type FieldSpec struct {
 	CreateIfNotPresent bool   `json:"create,omitempty" yaml:"create,omitempty"`
 }
 
-func updateLabels(o *fn.KubeObject, newLabels map[string]string, spec FieldSpec) error {
+func SetLabels(rl *fn.ResourceList) (bool, error) {
+	transformer := LabelTransformer{}
+	if err := transformer.Config(rl.FunctionConfig); err != nil {
+		rl.Results = append(rl.Results, fn.ErrorResult(err))
+		return false, err
+	}
+	if err := transformer.Transform(rl.Items); err != nil {
+		rl.Results = append(rl.Results, fn.ErrorResult(err))
+		return false, err
+	}
+
+	rl.Results = append(rl.Results, transformer.Results...)
+	// TODO: another way to pass result.
+	return true, nil
+}
+
+type LabelTransformer struct {
+	// Desired labels
+	NewLabels map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
+	// FieldSpecs is deprecated, please use AdditionalLabelFields instead.
+	FieldSpecs []FieldSpec `json:"fieldSpecs,omitempty" yaml:"fieldSpecs,omitempty"`
+	// AdditionalLabelFields is used to specify additional fields to add labels. TODO: deprecated soon
+	AdditionalLabelFields []FieldSpec `json:"additionalLabelFields,omitempty" yaml:"additionalLabelFields,omitempty"`
+	// Results is used to track labels that have been applied
+	Results fn.Results
+}
+
+// Config parse the functionConfig kubeObject to the fields in the LabelTransformer
+func (p *LabelTransformer) Config(o *fn.KubeObject) error {
+	// parse labels to NewLabels
+	switch {
+	case o.IsEmpty():
+		return fmt.Errorf("failed to configure function: `functionConfig` must be either a `ConfigMap` or `SetLabels`")
+	case o.IsGVK("", "v1", "ConfigMap"):
+
+		//var cm corev1.ConfigMap
+		//if err := o.As(&cm); err != nil {
+		//	return err
+		//}
+		//p.NewLabels = cm.Data
+		p.NewLabels = o.NestedStringMapOrDie("data")
+		//if len(p.NewLabels) == 0 {
+		//	return fmt.Errorf("`data` should not be empty")
+		//}
+	case o.IsGVK(fnConfigGroup, fnConfigAPIVersion, legacyFnConfigKind):
+		fallthrough
+	case o.IsGVK(fnConfigGroup, fnConfigAPIVersion, fnConfigKind):
+		p.NewLabels = o.NestedStringMapOrDie("labels")
+		if len(p.NewLabels) == 0 {
+			return fmt.Errorf("failed to configure function: input label list cannot be empty")
+		}
+	default:
+		return fmt.Errorf("unknown functionConfig Kind=%v ApiVersion=%v, expect `%v` or `ConfigMap`",
+			o.GetKind(), o.GetAPIVersion(), fnConfigKind)
+	}
+	// add default fields
+	if err := p.addDefaultLabelFields(); err != nil {
+		return err
+	}
+	// add additional fields
+	//if o.IsGVK(fnConfigGroup, fnConfigAPIVersion, fnConfigKind) || o.IsGVK(fnConfigGroup, fnConfigAPIVersion, legacyFnConfigKind) {
+	//	var add []FieldSpec
+	//	if _, err := o.Get(&add, "additionalLabelFields"); err != nil {
+	//		return err
+	//	}
+	//	p.AdditionalLabelFields = append(p.AdditionalLabelFields, add...)
+	//}
+	return nil
+}
+
+func (p *LabelTransformer) Transform(objects fn.KubeObjects) error {
+	for _, o := range objects {
+		for _, sp := range p.FieldSpecs {
+			if (sp.Group == "" && sp.Version == "" && sp.Kind == "") || o.IsGVK(sp.Group, sp.Version, sp.Kind) {
+				// generate msg
+				res, _ := json.Marshal(p.NewLabels)
+				newResult := fn.Result{
+					Message:     "set labels: " + string(res),
+					Severity:    "",
+					ResourceRef: nil,
+					Field: &fn.Field{
+						Path:          sp.Path,
+						CurrentValue:  nil, // values to be updated in setLabel()
+						ProposedValue: nil, // values to be updated in setLabel()
+					},
+					File: &fn.File{
+						Path:  o.PathAnnotation(),
+						Index: o.IndexAnnotation(),
+					},
+					Tags: nil,
+				}
+				err := updateLabels(o, sp.Path, p.NewLabels, sp.CreateIfNotPresent)
+				if err != nil {
+					return err
+				}
+				p.Results = append(p.Results, &newResult)
+			}
+		}
+	}
+	return nil
+}
+
+func updateLabels(o *fn.KubeObject, fieldPath string, newLabels map[string]string, create bool) error {
 	//TODO: should support user configurable field for labels
-	basePath := strings.Split(spec.Path, "/")
+	basePath := strings.Split(fieldPath, "/")
 	keys := make([]string, 0)
 	for k := range newLabels {
 		keys = append(keys, k)
@@ -96,7 +135,7 @@ func updateLabels(o *fn.KubeObject, newLabels map[string]string, spec FieldSpec)
 		if err != nil {
 			return err
 		}
-		if exist || spec.CreateIfNotPresent {
+		if exist || create {
 			if err = o.SetNestedString(val, newPath...); err != nil {
 				return err
 			}
@@ -106,11 +145,12 @@ func updateLabels(o *fn.KubeObject, newLabels map[string]string, spec FieldSpec)
 
 }
 
-func readDefaultFieldSpec() []FieldSpec {
+func (p *LabelTransformer) addDefaultLabelFields() error {
 	var defaultFieldSpecs []FieldSpec
-	err := yaml.Unmarshal([]byte(commonLabelFieldSpecs), &defaultFieldSpecs)
+	err := json.Unmarshal([]byte(commonLabelFieldSpecs), &defaultFieldSpecs)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	return defaultFieldSpecs
+	p.FieldSpecs = append(p.FieldSpecs, defaultFieldSpecs...)
+	return nil
 }
--- functions/go/set-labels/transformer/labels_test.go
+++ functions/go/set-labels/transformer/labels_test.go
@@ -3,23 +3,56 @@ package transformer
 import (
 	"fmt"
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	"sigs.k8s.io/yaml"
 	"testing"
 )
 
-func runTest(t *testing.T, config, input, expected string) {
-	var sl SetLabels
-	err := yaml.Unmarshal([]byte(config), &sl)
+func TestLabelTransformer_simple_ConfigMap(t *testing.T) {
+	functionConfig := `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: my-config
+data:
+  app: myApp
+  quotedBoolean: "true"
+  quotedFruit: "peach"
+  unquotedBoolean: true
+  env: production
+`
+	input := `
+apiVersion: apps/v1
+kind: ConfigMap
+metadata:
+  name: whatever
+`
+
+	expected := `
+apiVersion: apps/v1
+kind: ConfigMap
+metadata:
+  name: whatever
+  labels:
+    app: myApp
+    env: production
+    quotedBoolean: "true"
+    quotedFruit: peach
+    unquotedBoolean: "true"
+`
+
+	transformer := LabelTransformer{}
+	config, _ := fn.ParseKubeObject([]byte(functionConfig))
+	_ = transformer.Config(config)
+	result, _ := fn.ParseKubeObject([]byte(input))
+	err := transformer.Transform(fn.KubeObjects{result})
 	if err != nil {
 		return
 	}
-	in, _ := fn.ParseKubeObject([]byte(input))
-	sl.Run(nil, nil, fn.KubeObjects{in})
-	//fmt.Println(ctx)
+	fmt.Println(transformer.Results)
 	exp, _ := fn.ParseKubeObject([]byte(expected))
-	if exp.String() != in.String() {
+
+	if exp.String() != result.String() {
 		fmt.Println("Actual:")
-		fmt.Println(in)
+		fmt.Println(result)
 		fmt.Println("===")
 		fmt.Println("Expected:")
 		fmt.Println(exp)
@@ -27,58 +60,120 @@ func runTest(t *testing.T, config, input, expected string) {
 	}
 }
 
-func Test_simple(t *testing.T) {
-	config := `
-labels:
-  color: orange
-  name: apple
+func TestLabelTransformer_simple_ConfigMap2(t *testing.T) {
+	functionConfig := `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: my-config
+data:
+  app: myApp
+  quotedBoolean: "true"
+  quotedFruit: "peach"
+  unquotedBoolean: true
+  env: production
 `
 	input := `
 apiVersion: apps/v1
 kind: ConfigMap
 metadata:
   name: whatever
+  labels:
+    extra: nil
+    env: dev
 `
-	expected := `
-apiVersion: apps/v1
+
+	expected := `apiVersion: apps/v1
 kind: ConfigMap
 metadata:
   name: whatever
   labels:
-    color: orange
-    name: apple
-`
-	runTest(t, config, input, expected)
+    extra: nil
+    env: production
+    app: myApp
+    quotedBoolean: "true"
+    quotedFruit: peach
+    unquotedBoolean: "true"`
+
+	transformer := LabelTransformer{}
+	config, _ := fn.ParseKubeObject([]byte(functionConfig))
+	_ = transformer.Config(config)
+	result, _ := fn.ParseKubeObject([]byte(input))
+	err := transformer.Transform(fn.KubeObjects{result})
+	if err != nil {
+		return
+	}
+	exp, _ := fn.ParseKubeObject([]byte(expected))
+
+	if exp.String() != result.String() {
+		fmt.Println("Actual:")
+		fmt.Println(result)
+		fmt.Println("===")
+		fmt.Println("Expected:")
+		fmt.Println(exp)
+		t.Fatalf("Actual doesn't equal to expected")
+	}
 }
 
-func Test_Additional_Field(t *testing.T) {
-	config := `
+func TestLabelTransformer_simple_ConfigFile(t *testing.T) {
+	functionConfig := `
+apiVersion: fn.kpt.dev/v1alpha1
+kind: SetLabels
+metadata:
+  name: my-config
 labels:
   color: orange
-  name: apple
-additionalLabelFields:
-  - path: data/selector
-    kind: ConfigMap
-    create: true
+  fruit: apple
 `
+
 	input := `
 apiVersion: apps/v1
-kind: ConfigMap
-metadata:
-  name: whatever
-`
-	expected := `
-apiVersion: apps/v1
-kind: ConfigMap
+kind: MyResource
 metadata:
   name: whatever
   labels:
-    color: orange
-    name: apple
-data:
+    extra: nil
+    env: dev
+spec:
   selector:
-    color: orange
-    name: apple
+    labels:
+      fruit: apple
+      name: jemma
 `
-	runTest(t, config, input, expected)
+
+	expected := `
+apiVersion: apps/v1
+kind: MyResource
+metadata:
+  name: whatever
+  labels:
+    extra: nil
+    env: dev
+    color: orange
+    fruit: apple
+spec:
+  selector:
+    labels:
+      fruit: apple
+      name: jemma
+`
+
+	transformer := LabelTransformer{}
+	config, _ := fn.ParseKubeObject([]byte(functionConfig))
+	_ = transformer.Config(config)
+	result, _ := fn.ParseKubeObject([]byte(input))
+	err := transformer.Transform(fn.KubeObjects{result})
+	if err != nil {
+		return
+	}
+	//fmt.Println(result.String())
+	exp, _ := fn.ParseKubeObject([]byte(expected))
+	if exp.String() != result.String() {
+		fmt.Println("Actual:")
+		fmt.Println(result)
+		fmt.Println("===")
+		fmt.Println("Expected:")
+		fmt.Println(exp)
+		t.Fatalf("Actual doesn't equal to expected")
+	}
 }
