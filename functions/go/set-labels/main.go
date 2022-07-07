ff4a7577d0d368e1e9899232e975bb571daa8140
Yuanyi <zyuanyi@google.com>
2022-07-06 17:17:59 -0700

    package import regrouping

--- functions/go/set-labels/main.go
+++ functions/go/set-labels/main.go
@@ -15,9 +15,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/GoogleContainerTools/kpt-functions-catalog/functions/go/set-labels/transformer"
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	"os"
 )
 
 //nolint
