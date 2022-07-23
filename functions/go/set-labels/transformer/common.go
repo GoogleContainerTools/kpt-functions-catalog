9dc7b14cf98f212c9aa6f2c58985d2cc3d39c45b
Yuwen Ma <yuwenma@google.com>
2022-07-23 02:41:30 -0700

    modify set-labels import consts

--- functions/go/set-labels/transformer/common.go
+++ functions/go/set-labels/transformer/common.go
@@ -15,9 +15,7 @@
 package transformer
 
 const (
-	fnConfigGroup      = "fn.kpt.dev"
-	fnConfigAPIVersion = "v1alpha1"
-	fnConfigKind       = "SetLabels"
+	FnConfigKind       = "SetLabels"
 	fnDeprecateField   = "additionalLabelFields"
 )
 
