14871720f10107e0344d4e90b16e4e56545dbb5e
Yuanyi <zyuanyi@google.com>
2022-07-27 14:58:15 -0700

    revert

--- functions/go/set-labels/transformer/labels.go
+++ functions/go/set-labels/transformer/labels.go
@@ -99,9 +99,6 @@ func (p *LabelTransformer) setCommonSpecLabels(o *fn.KubeObject) error {
 
 // Transform updates the labels in the right path using GVK filter and other configurable fields
 func (p *LabelTransformer) Transform(objects fn.KubeObjects) error {
-	if objects.Len() == 0 || objects[0] == nil {
-		return fmt.Errorf("no resources found")
-	}
 	for _, o := range objects.WhereNot(func(o *fn.KubeObject) bool { return o.IsLocalConfig() }) {
 		// this label need to set for all GVK
 		if err := p.setMetadataForAll(o); err != nil {
